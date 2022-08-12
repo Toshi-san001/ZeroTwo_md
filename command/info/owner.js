3dd3bf36ac1d7ca1ba38ea3e7a90e677d57d0208
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-08-12 16:45:42 +0300

    Update owner.js

--- command/info/owner.js
+++ command/info/owner.js
@@ -5,6 +5,6 @@ module.exports = {
 	isSpam: true,
 	async run({ msg, conn }, { q, map, args }) {
 		var msga = await conn.sendContact(msg.from, config.owner, msg);
-		await conn.sendMessage(msg.from, { text: `Well... He is my owner... Maybe.. 🦄` }, {});
+		await conn.sendMessage(msg.from, { text: `Fortunatus Mokaya 🦄` }, {});
 	},
 };
