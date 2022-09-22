607835a9d5dd9b6bd9da0375e06bb30e6b7f0ba3
♕︎Toshi_さん♕︎ <91844428+Toshi-san001@users.noreply.github.com>
2022-09-23 02:48:45 +0530

    Update addprem.js

--- command/owner/addprem.js
+++ command/owner/addprem.js
@@ -5,12 +5,12 @@ module.exports = {
 	desc: "Add user into the premium database",
 	isSpam: true,
 	isOwner: true,
-	query: "Example :\n*#addprem* @tag person\n*#addprem* number\n\nExample : #addprem @tag 30d",
+	query: "Example :\n*+addprem* @tag person\n*+addprem* number\n\nExample : +addprem @tag 30d",
 	use: "@tag 30d",
 	async run({ msg, conn }, { q, map, args }) {
 		if (args.length < 2)
 			return msg.reply(
-				`Example :\n*#addprem* @tag someone\n*#addprem* number days\n\nExample : #addprem @tag 30d`
+				`Example :\n*+addprem* @tag someone\n*+addprem* number days\n\nExample : +addprem @tag 30d`
 			);
 		if (msg.mentions.length !== 0) {
 			for (let i = 0; i < msg.mentions.length; i++) {
